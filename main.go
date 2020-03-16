a6ce4b753804eb2dee9203c03562b02837877277
ciiiii <allen.ccccnm@gmail.com>
2020-03-16 21:08:25 +0800

    debug photo url

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/ciiiii/Go2SheeP_bot/pkg/cos"
 	"github.com/ciiiii/Go2SheeP_bot/pkg/translate"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
@@ -16,6 +17,10 @@ func main() {
 		token          = os.Getenv("TOKEN")
 		translateAppId = os.Getenv("TRANSLATE_APP_ID")
 		translateKey   = os.Getenv("TRANSLATE_KEY")
+		cosBucket      = os.Getenv("COS_BUCKET")
+		cosRegion      = os.Getenv("COS_REGION")
+		cosSecretId    = os.Getenv("COS_SECRET_ID")
+		cosSecretKey   = os.Getenv("COS_SECRET_KEY")
 	)
 
 	webhook := &tb.Webhook{
@@ -31,13 +36,15 @@ func main() {
 		panic(err)
 	}
 	tr := translate.NewTranslator(translateAppId, translateKey)
+	_ = cos.NewCos(cosBucket, cosRegion, cosSecretId, cosSecretKey)
+
 	bot.Handle(tb.OnText, func(m *tb.Message) {
 		fmt.Printf("%s: %s\n", m.Sender.Username, m.Text)
 		bot.Send(m.Sender, m.Text)
 	})
 
 	bot.Handle(tb.OnPhoto, func(m *tb.Message) {
-		bot.Send(m.Sender, m.Photo.FileURL)
+		bot.Send(m.Sender, fmt.Sprintf("https://api.telegram.org/bot%s/getFile?file_id=%s", token, m.Photo.FileID))
 	})
 
 	bot.Handle(tb.OnDocument, func(m *tb.Message) {
@@ -46,15 +53,28 @@ func main() {
 
 	bot.Handle("/en", func(m *tb.Message) {
 		if len(m.Payload) == 0 {
-			bot.Send(m.Sender, "⚠️input string is invalid.")
+			bot.Send(m.Sender, "⚠️ input string is invalid.")
 			return
 		}
 		result, err := tr.Translate("auto", "en", m.Payload)
 		if err != nil {
-			bot.Send(m.Sender, "👎translate failed.")
+			bot.Send(m.Sender, "👎 translate failed.")
 			return
 		}
-		bot.Send(m.Sender, fmt.Sprintf("👌:%s", result))
+		bot.Send(m.Sender, fmt.Sprintf("👌 %s", result))
+	})
+
+	bot.Handle("/cn", func(m *tb.Message) {
+		if len(m.Payload) == 0 {
+			bot.Send(m.Sender, "⚠️ input string is invalid.")
+			return
+		}
+		result, err := tr.Translate("auto", "en", m.Payload)
+		if err != nil {
+			bot.Send(m.Sender, "👎 translate failed.")
+			return
+		}
+		bot.Send(m.Sender, fmt.Sprintf("👌 %s", result))
 	})
 
 	bot.Start()
