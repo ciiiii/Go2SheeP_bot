98740c5e748bc1a81e2194b96b21712279ceaf4b
ciiiii <allen.ccccnm@gmail.com>
2020-03-24 20:25:33 +0800

    slepp after setting webhook

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"time"
+
 	"github.com/ciiiii/Go2SheeP_bot/bot"
 	"github.com/ciiiii/Go2SheeP_bot/config"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
 func main() {
-	//ok := bot.ManageWebHook("set")
-	//if !ok {
-	//	panic("can't set webhook")
-	//}
+	ok := bot.ManageWebHook("set")
+	if !ok {
+		panic("can't set webhook")
+	}
+	time.Sleep(5 * time.Second)
 	webHook := &tb.Webhook{
 		Listen:   ":" + config.Parser().Deploy.Port,
 		Endpoint: &tb.WebhookEndpoint{PublicURL: config.Parser().Bot.PublicUrl},
